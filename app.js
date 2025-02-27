9c4a705b2b63d6b11cbd3f597a2ab2a4873a12ea
193554832+Dani-DEV28@users.noreply.github.com <193554832+Dani-DEV28@users.noreply.github.com>
2025-02-27 15:23:30 -0800

    security check section A

--- app.js
+++ app.js
@@ -2,6 +2,12 @@
 import express from "express"
 import express from "mariadb"
 
+const pool = mariadb.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
+});
 
 // Function for connecting to the database
 async function connect() {
